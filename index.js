45bcd48efeb7f8219fbd206b231e8faaebb91009
Adesh Jadhav <adejad1234@gmail.com>
2024-02-15 19:59:12 +0530

    Implement login function

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ let person = {
     userName : "Adesh1461",
     password: "Adesh@2001"
 }
+
+function login (){
+    console.log("UserName : ", person.userName);
+    console.log("Password: ", person.password);
+}
